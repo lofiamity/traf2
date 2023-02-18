0097a7e98543286281afb0cc7d90598019c55d03
lofiamity <108965582+lofiamity@users.noreply.github.com>
2023-02-18 18:00:52 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+import random
+import time
+import requests
+
+# daftar link tujuan yang akan dijadwalkan
+links = ["https://www.contohlinktujuan1.com", "https://www.contohlinktujuan2.com", "https://www.contohlinktujuan3.com"]
+
+# fungsi untuk mengakses link secara acak
+def access_random_link():
+    # memilih link secara acak dari daftar links
+    link = random.choice(links)
+    try:
+        # mengakses link menggunakan library requests
+        response = requests.get(link)
+        print("Accessed link:", link, "with status code:", response.status_code)
+    except:
+        print("Failed to access link:", link)
+
+# menjadwalkan akses ke link setiap 5 detik
+while True:
+    access_random_link()
+    time.sleep(5)
