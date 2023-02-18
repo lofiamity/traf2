6df8c3f2ed1addf9dc6f3644b09e426af2305dbd
lofiamity <108965582+lofiamity@users.noreply.github.com>
2023-02-18 18:20:26 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import time
 import requests
 
 # daftar link tujuan yang akan dijadwalkan
-links = ["https://lofiamity-traf1-main-p5wzjz.streamlit.app/", "https://lofiamity-rembg-main-0h4y2j.streamlit.app/"]
+links = ["https://lofiamity-traf1-main-p5wzjz.streamlit.app/", "https://lofiamity-rem1bg-main-owrrs8.streamlit.app/"]
 
 # fungsi untuk mengakses link secara acak
 def access_random_link():
